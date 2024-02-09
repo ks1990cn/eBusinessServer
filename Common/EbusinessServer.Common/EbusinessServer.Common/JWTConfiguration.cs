f4ca541bf2a5174d624a153b6a271c349b26ccf3
Tanmay Seth <kushal.seth2010@gmail.com>
2024-02-09 17:17:06 +0530

    adding helper

--- Common/EbusinessServer.Common/EbusinessServer.Common/JWTConfiguration.cs
+++ Common/EbusinessServer.Common/EbusinessServer.Common/JWTConfiguration.cs
@@ -6,16 +6,19 @@ namespace EbusinessServer.Common
 {
     public static class JWTConfiguration
     {
-        static void CreateJWTConfiguration(WebApplicationBuilder builder)
+        static async Task CreateJWTConfigurationAsync(WebApplicationBuilder builder)
         {
+            string clientId = await ParameterHelper.GetParameterAsync("clientIdUserPool");
+            string region = await ParameterHelper.GetParameterAsync("region");
+            string userPoolId = await ParameterHelper.GetParameterAsync("userPoolId");
             builder.Services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(options =>
             {
-                options.Authority = "https://cognito-idp.{region}.amazonaws.com/{userPoolId}";
-                options.Audience = "{clientId}";
+                options.Authority = $"https://cognito-idp.{region}.amazonaws.com/{userPoolId}";
+                options.Audience = clientId;
             });
         }
     }
